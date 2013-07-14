c37ec77129bbac117626e9d84f6bd6cfa31699bc
Sheehan Toufiq <sheehantoufiq@gmail.com>
2013-07-14 01:28:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.readFileSync('./index.html', function (err, html) {
+	if (err) {
+		throw err;
+	}
+/*
 app.get('/', function(request, response) {
   response.send('Hello World 3!');
 });
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
