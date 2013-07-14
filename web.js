7c6cce62a4228af07d83bceb81ab026a7fb72689
Sheehan Toufiq <sheehantoufiq@gmail.com>
2013-07-14 01:35:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,15 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('./bitstarter/index.html', function (err, html) {
-	if (err) {
-		throw err;
-	}
-/*
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+ 
+// response.send('Hello World 3!');
+
+	fs.readFileSync('./bitstarter/index.html', function (err, html) {
+        	if (err) {
+                	throw err;
+        	}
 });
-*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
