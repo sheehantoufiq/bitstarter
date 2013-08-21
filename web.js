fbfa9ea9e301c37c38392a5577b90a3450915705
Sheehan Toufiq <sheehantoufiq@gmail.com>
2013-08-21 08:12:29 +0000

    Updated web.js and package.json

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
 
-    // response.send('Hello World 3!');
-    var fs = require('fs');
-    var buff = new Buffer (fs.readFileSync('index.html'), 'utf-8');
-    response.send(buff.toString());
+    var html  = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
